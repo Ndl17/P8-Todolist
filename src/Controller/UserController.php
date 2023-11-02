607c1b47507e12624dc5a2f240dc5c48d9e5f168
Ndl17 <juliengautiernadal@hotmail.com>
2023-11-02 23:05:41 +0100

    user creation implement
    dans user controller fonction pour creation d'un user
    creation d'un form pour la creation d'un user
    unicité de email dans entité
    maj base twig et button créer avec le lien vers fonction
    mise en place vue create

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\userCreationForm;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'user_list')]
+    #[Route('/users', name: 'user_list')]
     public function indexList(UserRepository $userRepository): Response
     {
                 //récupère toutes les user grâce à la méthode findAll()
@@ -19,4 +24,32 @@ class UserController extends AbstractController
             'users' => $users,
         ]);
     }
+
+    #[Route('/users/create', name: 'user_create')]
+public function create(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+{
+    $user = new User();
+    $form = $this->createForm(userCreationForm::class, $user);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        // encode the plain password
+        $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                $user,
+                $form->get('password')->getData()
+            )
+        );
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+        // do anything else you need here, like send an email
+
+        return $this->redirectToRoute('user_list');
+    }
+
+    return $this->render('user/create.html.twig', [
+        'userCreationForm' => $form->createView(),
+    ]);
+}
 }
