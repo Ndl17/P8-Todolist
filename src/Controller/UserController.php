bb99bc53e4f234f3da69fdd0f66d81735b89f4f7
Ndl17 <juliengautiernadal@hotmail.com>
2023-11-02 18:15:51 +0100

    read users implement
    UserController: récupération de la liste des user en provenance du repository et envoie des données vers la vue
    
    Vue: Mise en forme de la vue selon le projet d'origine, rajout dans base twig du bouton liste utilisateur non présent et mise des boutons dans le nav

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/user', name: 'user_list')]
+    public function indexList(UserRepository $userRepository): Response
     {
-        
+                //récupère toutes les user grâce à la méthode findAll()
+                $users = $userRepository->findAll();
+
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $users,
         ]);
     }
 }
