af2373bd28ed6c200dec5c40167d80040591f11d
Ndl17 <juliengautiernadal@hotmail.com>
2023-11-15 22:54:54 +0100

    task form optimisation
    réunir task edition et création en un seul form + modif vues

--- src/Form/TaskFormType.php
+++ src/Form/TaskFormType.php
@@ -12,7 +12,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class TaskEditionFormType extends AbstractType
+class TaskFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
